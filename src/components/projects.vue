a0cc28013a786d3c69ba7a585fd041e2a510debe
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2022-10-22 20:43:04 +0200

    add project

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -9,12 +9,17 @@
           <h4 class="project-title">WeChat Mini Program</h4>
 
           <div class="project-description">
-            WeChat Mini Program is an App without installing it on the
-            smartphone. Mini Program are lightweighted app, that can be opened
-            within WeChat App. For Dezan Shira & Associates I designed and
-            develop a Mini Program. This Mini Program contains information about
-            the company, services and offices around the world, especially in
-            South East Asia.
+            <p>
+              For Dezan Shira & Associates I designed and develop a Mini
+              Program. This Mini Program contains information about the company,
+              services and offices around the world, especially in South East
+              Asia.
+            </p>
+            <p>
+              WeChat Mini Program is an App without installing it on the
+              smartphone. Mini Program are lightweighted app, that can be opened
+              within WeChat App.
+            </p>
           </div>
 
           <ul class="project-tech-list">
@@ -67,14 +72,14 @@
 
           <div class="project-description">
             <p>
-              How to create a NFT gallery is part of a 10-week, self-paced
-              blockchain developer program to help me to build projects, provide
-              by Alchemy.
-            </p>
-            <p>
               Based on the contract address and/or collection address, the user
               can fetch fetching NFTs. Fetching NFTs is possible because of the
               use of the Alchemy NFT API.
+            </p>
+            <p>
+              How to create a NFT gallery is part of a 10-week, self-paced
+              blockchain developer program to help me to build projects, provide
+              by Alchemy.
             </p>
           </div>
 
@@ -121,6 +126,76 @@
       <div class="project-image">
         <a href="#">
           <img src="@/assets/img/nft-gallery.png" class="img" />
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row g-2">
+    <div class="col project pb-5">
+      <div class="project-content">
+        <div>
+          <p class="project-overline">Featured Project</p>
+
+          <h4 class="project-title">Build a NFT Marketplace</h4>
+
+          <div class="project-description">
+            <p>
+              The NFT marketplace is build with Vue, Bootstrap and Hardhat. With
+              Pinata IPFS, the user can upload any NFTs to the marketplace, so
+              that others users can buy that NFTs.
+            </p>
+
+            <p>
+              How to Build a NFT Marketplace from Scratch is part of a 10-week,
+              self-paced blockchain developer program to help me to build
+              projects, provide by Alchemy.
+            </p>
+          </div>
+
+          <ul class="project-tech-list">
+            <li>Vue</li>
+            <li>Bootstrap</li>
+            <li>Solidity</li>
+          </ul>
+
+          <div class="project-links">
+            <a
+              href="https://github.com/yohn-cheung/week7-create-nft-marketplace"
+              target="_blank"
+              aria-label="GitHub"
+              id="github"
+              class="btn btn-sm"
+              role="button"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-github"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+                />
+              </svg>
+              Github
+            </a>
+            <a
+              href="https://week7-create-nft-marketplace.vercel.app/"
+              target="_blank"
+              id="website"
+              class="btn btn-sm"
+            >
+              Website
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="project-image">
+        <a href="#">
+          <img src="@/assets/img/marketplace.png" class="img" />
         </a>
       </div>
     </div>
